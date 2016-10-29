1d422816d501a8f216c8c9a5a36e9e6a33b24af1
aiyou4love <aiyou4love@outlook.com>
2016-10-29 15:12:59 +0800

    u

---
+++ window/cmake/cmake.bat
@@ -0,0 +1,25 @@
+@echo off
+:::::::::::::::::::::初始化编译环境:::::::::::::::::::::
+C:
+cd "C:\Program Files (x86)\Microsoft Visual C++ Build Tools"
+call vcbuildtools.bat
+:::::::::::::::::::::跳转到脚本目录:::::::::::::::::::::
+%DRIVER%
+cd %PROJPATH%
+:::::::::::::::::::::跳转到项目目录:::::::::::::::::::::
+cd ../..
+::::::::::::::::::::::保存项目名称::::::::::::::::::::::
+for %%a in ("%cd%") do set SLNAME=%%~nxa
+:::::::::::::::::::跳转到编译中间目录:::::::::::::::::::
+cd develop
+:::::::::::::::::::跳转到编译项目目录:::::::::::::::::::
+if not exist %PROJNAME% (
+	mkdir %PROJNAME%
+)
+cd %PROJNAME%
+::::::::::::::::::::::::执行编译::::::::::::::::::::::::
+cmake %SLN_ROOT%/resource/cmake -D__SLNAME__=%SLNAME% -D__PROJNAME__=%PROJNAME%
+nmake
+nmake install
+::::::::::::::::::::::::编译完成::::::::::::::::::::::::
+@echo on
