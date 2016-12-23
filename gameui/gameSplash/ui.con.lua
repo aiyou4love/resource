2607c37ac73839cdaf4918cb61ccba21ebd20aab
aiyou4love <aiyou4love@outlook.com>
2016-12-23 21:16:26 +0800

    u

---
+++ gameui/gameSplash/ui.con.lua
@@ -0,0 +1,16 @@
+function runInit(nUiHandle)
+	setHandle(nUiHandle)
+end
+
+function showText()
+	mUiHandle:printText("header00")
+end
+
+function runShow()
+end
+
+function runTick()
+end
+
+function runClose()
+end
