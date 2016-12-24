a593cdeff287e456e5293ade4f4c5853f8a37f7c
aiyou4love <aiyou4love@outlook.com>
2016-12-24 08:49:48 +0800

    u

--- gameui/gameLeave/ui.con.lua
+++ gameui/gameLeave/ui.con.lua
@@ -8,12 +8,16 @@ function showText()
 	mUiHandle:printText("command0")
 	mUiHandle:printText("command1")
 	mUiHandle:printText("command2")
-	mUiHandle:printText("command3")
-	mUiHandle:printText("command4")
 	mUiHandle:printText("header00")
 end
 
 function runShow()
+end
+
+function runTick()
+end
+
+function showTick()
 end
 
 function runClose()
--- gameui/gameSplash/ui.con.lua
+++ gameui/gameSplash/ui.con.lua
@@ -3,13 +3,24 @@ function runInit(nUiHandle)
 end
 
 function showText()
-	mUiHandle:printText("header00")
 end
 
 function runShow()
+	mUiHandle:printText("header00")
 end
 
+local mTick = 10
 function runTick()
+	if mTick >= 0 then
+		mTick = mTick - 1
+	end
+end
+
+function showTick()
+	if mTick >= 0 then
+		print(mTick)
+	else
+	end
 end
 
 function runClose()
